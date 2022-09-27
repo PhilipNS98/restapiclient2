d925012dd82803c2364a77b1bec72676ec32c360
PhilipNS98 <90253330+PhilipNS98@users.noreply.github.com>
2022-09-27 10:30:02 +0200

    Connected

--- src/main/java/com/example/restapiclient/HomeController.java
+++ src/main/java/com/example/restapiclient/HomeController.java
@@ -34,7 +34,7 @@ public class HomeController {
     @PostMapping("/findFlag")
     public ResponseEntity<Flag> findFlag(@RequestBody FlagRequest flagRequest){
         System.out.println("findFlag called");
-        flagRequest.setRequest(" At P3. " + flagRequest.getRequest());
+        flagRequest.setRequest(" At Philip. " + flagRequest.getRequest());
         Flag flag = apiService.requestFlag(flagRequest);
         return new ResponseEntity<>(flag, HttpStatus.OK);
     }
